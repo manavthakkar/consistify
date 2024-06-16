8e42e2def761c29553b75e12ee4c8e058628e845
Manav Thakkar <manav.thakkar@tuhh.de>
2024-06-17 00:27:15 +0200

    Preprocess and warp the image

---
+++ test.py
@@ -0,0 +1,78 @@
+import cv2
+import numpy as np
+import utils
+
+# Load the image
+img = cv2.imread('habit-tracker.jpg')  
+
+
+# Preprocess the image
+# img = cv2.resize(img, (1024, 576)) 
+imgContours = img.copy()
+imgBlank = np.zeros_like(img) # Create a blank image with the same dimensions as the original image
+imgBiggestContours = img.copy() # for displaying the three biggest contours
+
+# Convert the image to grayscale
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+
+# Apply GaussianBlur to the image
+blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0) # (5, 5) is the kernel size and 0 is the standard deviation
+
+# Apply Canny edge detection
+canny_image = cv2.Canny(blurred_image, 10, 50) # 100 is the lower threshold and 200 is the upper threshold
+
+# Find the contours in the image
+contours, hierarchy = cv2.findContours(canny_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+cv2.drawContours(imgContours, contours, -1, (0, 255, 0), 3)
+
+# Get the rectangle contours
+rectCon = utils.rectContour(contours)
+# print("Number of rectangle contours: ",len(rectCon)) # Number of rectangle contours
+biggest_rectCon = utils.getCornerPoints(rectCon[0])
+second_biggest_rectCon = utils.getCornerPoints(rectCon[1])
+third_biggest_rectCon = utils.getCornerPoints(rectCon[2])
+
+if biggest_rectCon.size != 0 and second_biggest_rectCon.size != 0 and third_biggest_rectCon.size != 0:
+    cv2.drawContours(imgBiggestContours, biggest_rectCon, -1, (255, 50, 255), 20)
+    cv2.drawContours(imgBiggestContours, second_biggest_rectCon, -1, (255, 0, 0), 20)
+    cv2.drawContours(imgBiggestContours, third_biggest_rectCon, -1, (0, 0, 255), 20)
+
+    biggest_rectCon = utils.reorder(biggest_rectCon)
+    second_biggest_rectCon = utils.reorder(second_biggest_rectCon)
+    third_biggest_rectCon = utils.reorder(third_biggest_rectCon)
+
+    # Warp the image
+    pt1 = np.float32(biggest_rectCon)
+    pt2 = np.float32([[0, 0], [1024, 0], [0, 576], [1024, 576]])
+    matrix = cv2.getPerspectiveTransform(pt1, pt2)
+    imgWarpColored = cv2.warpPerspective(img, matrix, (1024, 576))
+    cv2.imshow('Warped Image', imgWarpColored)
+
+    # Warp the second biggest rectangle
+    ptS1 = np.float32(second_biggest_rectCon)
+    ptS2 = np.float32([[0, 0], [1024, 0], [0, 576], [1024, 576]])
+    matrixS = cv2.getPerspectiveTransform(ptS1, ptS2)
+    imgWarpColoredS = cv2.warpPerspective(img, matrixS, (1024, 576))
+    cv2.imshow('Warped Image Second', imgWarpColoredS)
+
+    # Warp the third biggest rectangle
+    ptT1 = np.float32(third_biggest_rectCon)
+    ptT2 = np.float32([[0, 0], [1024, 0], [0, 576], [1024, 576]])
+    matrixT = cv2.getPerspectiveTransform(ptT1, ptT2)
+    imgWarpColoredT = cv2.warpPerspective(img, matrixT, (1024, 576))
+    cv2.imshow('Warped Image Third', imgWarpColoredT)
+
+
+
+
+# Display the image
+#cv2.imshow('Original Image', img)
+
+imgArray = ([img, gray_image, blurred_image, canny_image],
+            [imgContours, imgBiggestContours, imgBlank, imgBlank])
+
+# Display the stacked images
+stacked_images = utils.stackImages(imgArray, 0.5)
+cv2.imshow('Stacked Images', stacked_images)
+
+cv2.waitKey(0)
---
+++ utils.py
@@ -0,0 +1,87 @@
+import cv2
+import numpy as np
+
+def stackImages(imgArray,scale,lables=[]):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+            hor_con[x] = np.concatenate(imgArray[x])
+        ver = np.vstack(hor)
+        ver_con = np.concatenate(hor)
+    else:
+        for x in range(0, rows):
+            imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        hor_con= np.concatenate(imgArray)
+        ver = hor
+    if len(lables) != 0:
+        eachImgWidth= int(ver.shape[1] / cols)
+        eachImgHeight = int(ver.shape[0] / rows)
+        #print(eachImgHeight)
+        for d in range(0, rows):
+            for c in range (0,cols):
+                cv2.rectangle(ver,(c*eachImgWidth,eachImgHeight*d),(c*eachImgWidth+len(lables[d][c])*13+27,30+eachImgHeight*d),(255,255,255),cv2.FILLED)
+                cv2.putText(ver,lables[d][c],(eachImgWidth*c+10,eachImgHeight*d+20),cv2.FONT_HERSHEY_COMPLEX,0.7,(255,0,255),2)
+    return ver
+
+
+def rectContour(contours): 
+    '''
+    It takes the contours as input and returns the rectangle contours
+
+    '''
+    rectCon = []                        # List to store all rectangle contours
+    for i in contours:
+        area = cv2.contourArea(i)
+        print(area)
+        if area > 50:                    # Minimum area of rectangle
+            peri = cv2.arcLength(i, True) # Calculate the perimeter of the contour (True: closed contour)
+            approx = cv2.approxPolyDP(i, 0.02 * peri, True) # Approximate the polygon (how many corner points it has) (contour, resolution, closed)
+            if len(approx) == 4: # If the contour has 4 corners
+                rectCon.append(i)
+    rectCon = sorted(rectCon, key=cv2.contourArea,reverse=True) # Sort the contours by area in descending order
+    #print(len(rectCon))
+    return rectCon
+
+def getCornerPoints(cont):
+    """
+    This function takes the contour as input and returns the corner points of the contour
+
+    """
+    peri = cv2.arcLength(cont, True) # LENGTH OF CONTOUR
+    approx = cv2.approxPolyDP(cont, 0.02 * peri, True) # APPROXIMATE THE POLY TO GET CORNER POINTS
+    return approx
+
+def reorder(myPoints):
+    """
+    This function takes the points as input and returns the arranged points (of the rectangle)
+    for example, it will return the points in the following order:
+    [top-left, top-right, bottom-left, bottom-right]
+    
+    """
+    myPoints = myPoints.reshape((4, 2)) # REMOVE EXTRA BRACKET
+    #print(myPoints)
+    myPointsNew = np.zeros((4, 1, 2), np.int32) # NEW MATRIX WITH ARRANGED POINTS
+    add = myPoints.sum(1)
+    #print(add)
+    #print(np.argmax(add))
+    myPointsNew[0] = myPoints[np.argmin(add)]  #[0,0]    # origin point has the smallest sum
+    myPointsNew[3] =myPoints[np.argmax(add)]   #[w,h]    # w+h gives the largest sum
+    diff = np.diff(myPoints, axis=1)
+    myPointsNew[1] =myPoints[np.argmin(diff)]  #[w,0]    
+    myPointsNew[2] = myPoints[np.argmax(diff)] #[h,0]    
+
+    return myPointsNew
